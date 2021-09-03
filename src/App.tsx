d3460a93c23baaa91353d9f0f4973e4c04c17ed1
Di <altusj@gmail.com>
2021-09-02 22:54:15 -0400

    working API for hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
-//import './App.css';
 
 interface IPost {
   id: number;
@@ -16,63 +15,36 @@ type SetPostsHook = (posts: IPost[]) => void;
 type SetLoadingHook = (loading: boolean) => void;
 
 function App() {
-  const [posts, setPosts]: [IPost[], SetPostsHook] = useState(defaultPosts);
-  const [loading, setLoading]: [boolean, SetLoadingHook] =
-    useState(defaultLoadingState);
-  const [error, setError]: [string, SetErrorHook] = useState("");
+  const [posts, setPosts]: [IPost[], SetPostsHook] =
+    React.useState(defaultPosts);
+  const [loading, setLoading]: [boolean, (loading: boolean) => void] =
+    React.useState<boolean>(true);
+  const [error, setError]: [string, (error: string) => void] =
+    React.useState("");
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      console.log("got response, data is", response.data);
+      setPosts(response.data);
+      setLoading(false);
+      console.log("posts state is ", posts);
+      console.log("loading state is ", loading);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <ul className="posts">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
 }
-
-// const App = () => {
-//   const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] =
-//     React.useState(defaultPosts);
-//   const [loading, setLoading]: [boolean, (loading: boolean) => void] =
-//     React.useState<boolean>(true);
-//   const [error, setError]: [string, (error: string) => void] =
-//     React.useState("");
-// };
-
-// const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] =
-//   React.useState(defaultPosts);
-// const [loading, setLoading]: [boolean, (loading: boolean) => void] =
-//   React.useState<boolean>(true);
-// const [error, setError]: [string, (error: string) => void] = React.useState("");
-
-// React.useEffect(() => {
-//   axios.get<IPost[]>("https://jsonplaceholder.typicode.com/posts", {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-// }, []);
-
-// React.useEffect(() => {
-//   axios
-//     .get<IPost[]>(...)
-//     .then(response => {
-//       setPosts(response.data);
-//       setLoading(false);
-//     });
-// }, []);
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
